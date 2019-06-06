93871d1e3505b24199cc26b97edb35446d778c80
Thu Tran <51333005+v-thutra@users.noreply.github.com>
2019-06-06 13:30:37 -0500

    Add files via upload

---
+++ C#/Mars.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mars
+{
+    class Mars
+    {
+        static bool isValid(int year)
+        {
+            int month = 12 * (year - 1);
+            for (int i = 1; i < 13; i++)
+            {
+                if((month+i-2)%26 == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int inputInt = Convert.ToInt32(input);
+            if (isValid(inputInt))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
