d1c7d8ad376fef32c6291a5e47e01c9cea4ec64a
Thu Tran <51333005+v-thutra@users.noreply.github.com>
2019-06-07 10:24:21 -0500

    Add files via upload

---
+++ C#/Bijele.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bijele
+{
+    /// <summary>
+    /// Solution to problem Bijele found at https://open.kattis.com/submissions/4240300
+    /// </summary>
+    class Bijele
+    {
+        /// <summary>
+        /// main function takes in user input
+        /// and output a series of numbers of pieces needed for a complete set
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split(' ');
+            int[] enough = { 1, 1, 2, 2, 2, 8 };
+            for (int i = 0; i <6; i++)
+            {
+                int ans = enough[i] - Convert.ToInt32(tokens[i]);
+                Console.Write(ans);
+                if (i != 5)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
